07228813c27fa9bac9ddaab4b2c60b7ed206ef6e
alexei365 <alexandernwazue@gmail.com>
2024-05-30 02:58:30 -0700

    complete work on calculator

---
+++ script.js
@@ -0,0 +1,23 @@
+// script.js
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function deleteLast() {
+    let display = document.getElementById('display');
+    display.value = display.value.slice(0, -1);
+}
+
+function appendToDisplay(value) {
+    let display = document.getElementById('display');
+    display.value += value;
+}
+
+function calculateResult() {
+    let display = document.getElementById('display');
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        alert('Invalid Expression');
+    }
+}
